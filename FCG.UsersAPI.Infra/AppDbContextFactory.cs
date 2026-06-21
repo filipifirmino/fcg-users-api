ca69e590f0458e7877c125cd4f081fb001f19d29
Luís <luisbocchinii@gmail.com>
2026-06-21 10:10:38 -0300

    feat: criação do banco e migrations

--- FCG.UsersAPI.Infra/AppDbContextFactory.cs
+++ FCG.UsersAPI.Infra/AppDbContextFactory.cs
@@ -8,9 +8,11 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=FCG;Username=postgree;Password=postgree")
-                      .UseSnakeCaseNamingConvention();
+
+        optionsBuilder.UseNpgsql(
+                "Host=localhost;Port=5433;Database=fcg_users;Username=postgree;Password=postgree")
+            .UseSnakeCaseNamingConvention();
+
         return new AppDbContext(optionsBuilder.Options);
     }
 }
-
---
+++ FCG.UsersAPI.Infra/Migrations/20260621130207_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCG.UsersAPI.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    email = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    role = table.Column<int>(type: "integer", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
